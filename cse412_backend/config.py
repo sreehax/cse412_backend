c9c90ff5aa5e9c9dfdf27fc700d3707f87d7616d
Sreehari Sreedev <me@ssree.dev>
2024-12-02 00:07:45 +0100

    Use postgres user

--- cse412_backend/config.py
+++ cse412_backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    user = os.getenv('USER')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{user}@localhost:5432/cse412')
+    #user = os.getenv('USER')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://postgres@localhost:5432/cse412')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
