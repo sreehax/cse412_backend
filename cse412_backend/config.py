798cce5358b399e04e075801ed8bafeb6d86135b
Sreehari Sreedev <me@ssree.dev>
2024-12-02 04:13:58 +0100

    unbreak running backend locally

--- cse412_backend/config.py
+++ cse412_backend/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    #user = os.getenv('USER')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://postgres@localhost:5432/cse412')
+    user = os.getenv('USER')
+    user = 'postgres' if user == 'root' else user
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'postgresql://{user}@localhost:5432/cse412')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
