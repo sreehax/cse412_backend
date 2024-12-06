128af1de97ff9069a117a58a9b4bea2ad66fcac6
rswart0604 <rswart0604@gmail.com>
2024-12-06 13:20:40 -0700

    can now submit an order and it will add to orders table and also add to ingredients stuff

--- cse412_backend/backend.py
+++ cse412_backend/backend.py
@@ -60,6 +60,16 @@ def bussinesscontact(contnum):
         return '', 404
     return jsonify(d[0])
 
+@app.route("/businesscontact/suppliers", methods=['GET'])
+def businesscontact_supplierlist():
+    result = db.session.execute(text("SELECT consupnum as supnum, contFName AS fname, contLName AS lname, contEmail AS email, "
+                                         "contWebsite AS website, contPhone AS phone FROM "
+                                     "businesscontact, supplierconnects WHERE concontnum = contnum"))
+    d = result_to_dict(result)
+    if len(d) < 1:
+        return '', 200
+    return jsonify(d)
+
 
 @app.route("/employeelist/<locnum>", methods=['GET'])
 def employee_list(locnum):
@@ -149,17 +159,58 @@ def get_ingredient(locnum):
     return jsonify(d)
 
 # add an order
-# todo finish this
+
+NUM_TO_ING = dict(zip(list(range(1,11)), ['Carrot', 'Basmati Rice', 'Green Peas', 'Garlic',
+                              'Onion', 'Eggs', 'Corn Cob', 'Salt Container', 'Soy Sauce Bottle', 'Parsley']))
+NUM_TO_TYPE = dict(zip(list(range(1,11)), ['Pounds', 'Pounds', 'Pounds', 'Pounds', 'Pounds', 'Dozens', 'Pounds',
+                                           'Count', 'Count', 'Pounds']))
+
 @app.route("/order/<locnum>", methods=['POST'])
 def add_order(locnum):
-    if request.is_json:
+    # i did not want to make ordnum serial so i just get the maxval of it and increment it!
+    # print(db.session.execute(text("SELECT MAX(ordnum) FROM orders;")).fetchall()[0][0])
+    try:
+        max_val = int(db.session.execute(text("SELECT MAX(ordnum) FROM orders;")).fetchall()[0][0])
         r = request.get_json()
+        print(r)
         ingredients = r.get('ingredients')
-        list_ingredients = ingredients.split(',')
-        num_each = [int(x) for x in r.get('num_each').split(',')]
-        result = db.session.execute("INSERT INTO orders (ordtotal, orddelivered, ordtime, "
+        ingredients_list = ingredients.split(',') if ',' in ingredients else [ingredients]
+        supnum = r.get('supnum')
+        result = db.session.execute(text("INSERT INTO orders (ordtotal, orddelivered, ordtime, "
                                     "orddate, ordingnumlist, ordsupnum, ordlocnum, ordnum) VALUES "
-                                    "(:total, '3', '12:00', '12/06/24', :numlist, :supnum, :locnum, )")
+                                    "(10, '3', '12:00', '12/06/24', :ingredients, :supnum, :locnum, :ordnum)"),
+                                    {'ingredients': ingredients, 'supnum': supnum, 'locnum': locnum,
+                                     'ordnum': str(max_val+1)})
+        db.session.commit()
+
+        for ing in ingredients_list:
+            result = db.session.execute(text("SELECT ingqty, ingmeasurementtype FROM ingredient WHERE ingnum = :ing AND locnum = :locnum "
+                                             "AND supnum = :supnum"),
+                                        {'ing': ing, 'locnum': locnum, 'supnum': supnum, })
+            d = result_to_dict(result)
+            if len(d) > 0:
+                # update this row
+                ingqty = int(d[0]['ingqty'])
+                db.session.execute(text("UPDATE ingredient SET ingqty = :ingqty WHERE ingnum = :ingnum AND supnum = :supnum AND"
+                                        " locnum = :locnum"),
+                                   {'ingqty': ingqty+10,'ingnum': ing, 'supnum': supnum, 'locnum': locnum})
+                db.session.commit()
+            else:
+                # add a new row
+                db.session.execute(text("INSERT INTO ingredient (ingmeasurementtype, ingname, expdate, ingqty, ingnum,"
+                                        " supnum, locnum) VALUES (:ingtype, :ingname, '12/7/24', '10', :num, :supnum, :locnum)"),
+                                   {'ingtype': NUM_TO_TYPE[int(ing)], 'ingname': NUM_TO_ING[int(ing)], 'num': ing,
+                                    'supnum': supnum, 'locnum': locnum})
+                print('hello, inserting')
+                db.session.commit()
+        r = db.session.execute(text("SELECT * FROM ingredient"))
+        print(r)
+        print(r.fetchall())
+        return '', 200
+    except Exception as e:
+        raise e
+        return '', 500
+
 
 
 def main():
